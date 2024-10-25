7376461e81e9ee4de1ae2412d2f2c4266ff47185
Reanike <88022428+Reanike@users.noreply.github.com>
2024-10-25 22:31:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/souwei168/luci-app-store.git
-git clone https://github.com/animegasan/luci-app-quickstart.git
-git clone https://github.com/sirpdboy/luci-app-cupsd.git
+#git clone https://github.com/souwei168/luci-app-store.git
+#git clone https://github.com/animegasan/luci-app-quickstart.git
+#git clone https://github.com/sirpdboy/luci-app-cupsd.git
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
