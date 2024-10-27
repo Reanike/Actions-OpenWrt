2c79f7506ca9a5a13950826c942f8cb27b1bfc1a
Reanike <88022428+Reanike@users.noreply.github.com>
2024-10-27 17:08:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 #git clone https://github.com/linkease/istore.git
 #git clone https://github.com/animegasan/luci-app-quickstart.git
 #git clone https://github.com/sirpdboy/luci-app-cupsd.git
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/small-package.git package/small-packages
