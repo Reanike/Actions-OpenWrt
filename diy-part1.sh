72d9f3c7f0c37fe129b702419cd2beb8d15dec94
Reanike <88022428+Reanike@users.noreply.github.com>
2024-11-03 12:27:01 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
-#echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 #git clone https://github.com/kenzok8/small-package.git package/small-package
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
